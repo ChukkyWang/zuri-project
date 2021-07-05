3a3888b1c19498c913038d16daadbe10049d39ad
Chukky Eze <ezechukwukae@gmail.com>
2021-07-05 23:38:57 +0100

    zuri products

--- src/zuri-app/components/index.js
+++ src/zuri-app/components/index.js
@@ -2,11 +2,18 @@ import React from 'react'
 import '../assets/style/home/style.css'
 import Logo from '../assets/images/zuri-logo-full.svg'
 
+
+//Product Images
+import Training from '../assets/images/training.svg'
+import Internship from '../assets/images/internship.svg'
+import Talent from '../assets/images/talent.svg'
+
 function Home() {
     return (    
         <div className="zuri_home">
            <Buzz/>
            <Products/>
+           <ZuriFooter/>
         </div>
     )
 }
@@ -46,13 +53,61 @@ function Buzz() {
 
 function Products() {
     const products = ([
-        
+        {
+            item: 'Zuri Training',
+            logo: Training,
+            long: "We understand a complete beginner cannot become a professional after a few weeks, the aim here is to get you familiar enough with each the tools such that you’ll be able to continue learning on your own with minimal support.",
+            click: "I want to be trained >"
+        },
+        {
+            item: 'Zuri Internship',
+            logo: Internship,
+            long: "We place you in a work simulation. We give you tasks every week, the tasks ranges from easy to hard as you progress from stage to stage, making it to the final stage means you are ready for actual work.",
+            click: "I want to join the internship >"
+        },
+        {
+            item: 'Zuri Talent',
+            logo: Talent,
+            long: "Keeping pace with projects and being on the look out for the extremely talented individuals has become ever more challenging. Here is where Zuri Team comes into play to bring the best suited talent for your company",
+            click: "I am looking for talent >"
+        }
     ])
     return(
-        <section>
+        <section className="product_itself">
             <div className="product_header">
                 <h5>BY ZURI Team</h5>
                 <h3>We cater for every level of <br/> expertise and needs in the industry.</h3>
+            </div>
+            <div className="product_specifics">
+                {
+                    products.map((product) => (
+                        <div key={product.item} className="product_type_card">
+                            <img src={product.logo} alt={product.item} />
+                            <p>{product.long}</p>
+                            <h6>
+                                <a href="http://">
+                                    {product.click}
+                                </a>
+                            </h6>
+                        </div>
+                    ))
+                }
+            </div>
+        </section>
+    )
+}
+
+
+
+//Footer Section
+function ZuriFooter() {
+    return(
+        <section>
+            <div className="contact_links">
+
+            </div>
+            <div className="copyright">
+
             </div>
         </section>
     )
